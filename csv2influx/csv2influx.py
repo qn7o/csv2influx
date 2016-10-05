d734c30d2758516ec9737135478f0659970590b3
Antonin Bourguignon <antonin.bourguignon@gmail.com>
2016-10-06 01:11:50 +0200

    Refactoring

--- csv2influx/csv2influx.py
+++ csv2influx/csv2influx.py
@@ -71,9 +71,90 @@ def sanitize_tag(value):
 
 def sanitize_field(value):
     """
-    Field keys are always strings and follow the same syntactical rules as described above for tag keys and values.
+    Field keys are always strings and follow the same syntactical rules as
+    described above for tag keys and values.
     """
     return sanitize_tags(value)
+
+
+def csv_dialect_to_str(dialect):
+    """
+    Make CSV dialect human readable.
+    """
+    return ' // '.join(['%s: %s' % (k, v) for k, v in dialect.__dict__.iteritems() if not k.startswith('_')])
+
+
+def arrow_ts_to_nano_ts(ts):
+    """
+    Extremely naïvely convert any arrow-ready timestamp into "nano seconds" timestamp.
+    Notice the quotation marks around "nano seconds".
+    """
+    t = arrow.get(arguments['--timestamp'])
+
+    # Mimic nano seconds like a retard
+    return ("%.6f" % t.float_timestamp).replace('.', '') + '000'
+
+
+def process_input_file(input_path, output_path, arguments):
+    with open(input_path, 'rb') as input_file, open(output_path % {'basename': os.path.basename(input_path)}, 'ab+') if output_path else tempfile.TemporaryFile() as output_file:
+        has_header = csv.Sniffer().has_header(input_file.readline())
+        input_file.seek(0)
+        dialect = csv.Sniffer().sniff(input_file.readline())
+        input_file.seek(0)
+        logging.info('CSV sniffer results: %s' % 'header detected' if has_header else 'no header detected')
+        logging.info('CSV sniffer results: %s' % csv_dialect_to_str(dialect))
+
+        csvreader = csv.reader(input_file, dialect)
+        if not has_header:
+            raise Exception('csv2influx needs CSV headers to properly match and write fields\' and tags\' labels')
+
+        # Extract header labels
+        labels = [s.lower() for s in csvreader.next()]
+
+        # Match field columns against provided arguments and convert labels into indexes
+        field_columns_indexes = [labels.index(s.lower()) for s in arguments['--field-columns'].split(',')]
+
+        # Match tag columns against provided arguments and convert labels into indexes
+        if arguments['--tag-columns'] == '*':
+            tag_columns_indexes = filter(lambda i: i not in field_columns_indexes, range(0, len(labels)))
+        elif arguments['--tag-columns']:
+            tag_columns_indexes = [labels.index(s.lower()) for s in arguments['--tag-columns'].split(',')]
+        else:
+            tag_columns_indexes = []
+
+        logging.debug('Fields indexes: %s' % field_columns_indexes)
+        logging.debug('Tags indexes: %s' % tag_columns_indexes)
+
+        # Sanitize labels since these values will be used as keys
+        sanitized_labels = [sanitize_tag(s) for s in labels]
+
+        # Using a buffer instead of writing directly to the file is more efficient
+        # when writing to a file *and* to a database at the same time
+        buf = cStringIO.StringIO()
+        for i, row in enumerate(csvreader):
+            measurement = sanitize_measurement(arguments['--measurement']),
+            tag_set = ','.join(['%s=%s' % (sanitized_labels[k], sanitize_tag(row[k])) for k in tag_columns_indexes])
+            field_set = ','.join(['%s=%s' % (sanitized_labels[l], sanitize_tag(row[l])) for l in field_columns_indexes])
+            nano_timestamp = arrow_ts_to_nano_ts(arguments['--timestamp']) if arguments['--timestamp'] else None
+
+            buf.write('%s,%s %s%s\n' % (
+                measurement,
+                tag_set,
+                field_set,
+                ' ' + nano_timestamp if nano_timestamp else '')
+            )
+
+        buf.seek(0)
+        shutil.copyfileobj (buf, output_file)
+        logging.info('Wrote %d lines to file %s' % (i, output_file.name))
+
+        if arguments['--load-url']:
+            r = requests.post(
+                url=arguments['--load-url'],
+                data=buf.getvalue(),
+                headers={'Content-Type': 'application/x-www-form-urlencoded'}
+            )
+            logging.info('Wrote %d lines to database %s' % (i, arguments['--load-url']))
 
 
 if __name__ == '__main__':
@@ -93,57 +174,7 @@ if __name__ == '__main__':
         logging.debug('Output path is empty: will not generate any output')
         output_path = None
 
+    for i, input_path in enumerate(glob(arguments['PATH'])):
+        logging.info('-- Processing file %d/%d: %s --' % (i+1, count_input_files, os.path.basename(input_path)))
 
-    for i, p in enumerate(glob(arguments['PATH'])):
-        logging.info('-- Processing file %d/%d: %s --' % (i+1, count_input_files, os.path.basename(p)))
-
-        with open(p, 'rb') as input_file, open(output_path % {'basename': os.path.basename(p)}, 'ab+') if output_path else tempfile.TemporaryFile() as output_file:
-            csvreader = csv.reader(input_file, delimiter=';')
-
-            if not arguments.get('--no-header', False):
-                # Extract header
-                header = [s.lower() for s in csvreader.next()]
-
-                # Convert field columns names into indexes
-                field_columns_indexes = [header.index(s.lower()) for s in arguments['--field-columns'].split(',')]
-
-                # Convert tag columns names into indexes
-                if arguments['--tag-columns'] == '*':
-                    tag_columns_indexes = filter(lambda i: i not in field_columns_indexes, range(0, len(header)))
-                else:
-                    tag_columns_indexes = [header.index(s.lower()) for s in arguments['--tag-columns'].split(',')]
-
-            logging.debug('Header: %s' % header)
-            logging.debug('Fields_indexes: %s' % field_columns_indexes)
-            logging.debug('Tags_indexes: %s' % tag_columns_indexes)
-
-            # Using a buffer instead of writing directly to the file helps to save some memory when writing to a file
-            # *and* to a database at the same time
-            buf = cStringIO.StringIO()
-            for j, row in enumerate(csvreader):
-                # Sanitize header since these values will be used as keys
-                sanitized_header = [sanitize_tag(s) for s in header]
-
-                key = '%s,%s' % (
-                    sanitize_measurement(arguments['--measurement']),
-                    ','.join(['%s=%s' % (sanitized_header[k], sanitize_tag(row[k])) for k in tag_columns_indexes])
-                )
-
-                fields = ','.join(['%s=%s' % (sanitized_header[l], sanitize_tag(row[l])) for l in field_columns_indexes])
-
-                nano_timestamp = None
-                if arguments['--timestamp']:
-                    t = arrow.get(arguments['--timestamp'])
-
-                    # Mimic nano seconds like a retard
-                    nano_timestamp = ("%.6f" % t.float_timestamp).replace('.', '') + '000'
-
-                buf.write('%s %s%s\n' % (key, fields, ' ' + nano_timestamp if nano_timestamp else ''))
-
-            buf.seek(0)
-            shutil.copyfileobj (buf, output_file)
-            logging.info('Wrote %d lines to %s' % (j, output_file.name))
-
-            if arguments['--load-url']:
-                r = requests.post(arguments['--load-url'], data=buf.getvalue(), headers={'Content-Type': 'application/x-www-form-urlencoded'})
-                logging.info('Wrote %d lines to %s' % (j, arguments['--load-url']))
+        process_input_file(input_path, output_path, arguments)

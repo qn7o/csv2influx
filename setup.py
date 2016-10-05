d231b1e926fe81036aa733a45674c28fec75f12a
Antonin Bourguignon <antonin.bourguignon@gmail.com>
2016-10-06 01:06:07 +0200

    Make it a proper python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='csv2influx',
+    version='0.1',
+    description='Write CSV data into InfluxDB thanks to Influx\'s line protocol syntax',
+    url='https://github.com/qn7o/csv2influx',
+    author='Antonin Bourguignon',
+    author_email='antonin.bourguignon@gmail.com',
+    license='MIT',
+    packages=['csv2influx'],
+    install_requires=[
+        'docopt',
+        'arrow',
+        'requests',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'csv2influx = csv2influx:main'
+        ],
+    },
+    zip_safe=False)

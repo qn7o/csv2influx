b96f5a78dba45466d9d67e00116b62444b9265dc
Antonin Bourguignon <antonin.bourguignon@gmail.com>
2017-09-14 17:28:26 +0200

    Use lineprotocol as a dependency

--- csv2influx/__init__.py
+++ csv2influx/__init__.py
@@ -1,3 +1,2 @@
 # -*- coding: utf-8 -*-
 from csv2influx import csv_dialect_to_str, arrow_string_to_nano_ts, process_input_file
-from lineprotocol import LineProtocolExporter
--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     install_requires=[
         'docopt',
         'arrow',
+        'lineprotocol',
         'requests',
     ],
     entry_points = {
